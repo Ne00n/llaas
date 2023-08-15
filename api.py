2fb2cf6e7222d1420a0e2ca797ca272b53e7c9e5
Neoon <neo@tiefkuehler-madness.me>
2023-08-15 22:25:09 +0200

    switch to gunicorn

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 import json, pyasn, sqlite3, time, re
 from pathlib import Path
 
+inboundQueue = []
 def validateToken(token=''):
     for name,details in config['workers'].items():
         if details['token'] == token: return True
@@ -38,30 +39,36 @@ def index(token=''):
 def index(request=''):
     if len(request) > 100: return HTTPResponse(status=414, body={"data":"way to fucking long"})
     request = request.replace("/","")
-    ipv4 = re.findall("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$",request, re.MULTILINE)
+    ipv4 = ipRegEx.findall(request)
     if ipv4:
         asndata = asndb.lookup(ipv4[0])
         if asndata[0] is None: return HTTPResponse(status=400, body={"data":"invalid IPv4"})
-        connection = sqlite3.connect("file:subnets?mode=memory&cache=shared", uri=True)
+        connection = sqlite3.connect("file:subnets?mode=memory&cache=shared", uri=True, isolation_level=None, timeout=10)
+        connection.execute('PRAGMA journal_mode=WAL;')
+        connection.commit()
         response = list(connection.execute("SELECT * FROM requests WHERE subnet = ?",(asndata[1],)))
-        expiry = int(time.time()) + 60
         if not response:
-            connection.execute(f"INSERT INTO requests VALUES ('{asndata[1]}','0','{expiry}')")
+            expiry = int(time.time()) + 60
+            connection.execute(f"INSERT INTO requests VALUES ('{asndata[1]}','{ipv4[0]}','0','{expiry}')")
             connection.commit()
-            connection.close()
-            return HTTPResponse(status=200)
-        return HTTPResponse(status=200, body={"subnet":response[0][0]})
+        response = list(connection.execute("SELECT * FROM requests WHERE subnet = ?",(asndata[1],)))
+        connection.close()
+        return HTTPResponse(status=200, body={"subnet":response[0][0],"status":response[0][2]})
     else:
         return HTTPResponse(status=400, body={"data":"invalid IPv4"})
 
 print("Preparing sqlite3")
-connection = sqlite3.connect("file:subnets?mode=memory&cache=shared", uri=True)
-connection.execute("""CREATE TABLE requests (subnet, status, expiry)""")
+connection = sqlite3.connect("file:subnets?mode=memory&cache=shared", uri=True, isolation_level=None)
+connection.execute("""CREATE TABLE requests (subnet, ip, status, expiry)""")
 connection.execute("""CREATE TABLE results (subnet, worker, latency, FOREIGN KEY(subnet) REFERENCES requests(subnet) ON DELETE CASCADE)""")
+connection.execute('PRAGMA journal_mode=WAL;')
+connection.commit()
 print("Loading config")
 with open('api.json') as f: config = json.load(f)
 print("Loading pyasn")
 asndb = pyasn.pyasn('asn.dat')
+print("Preparing regex")
+ipRegEx = re.compile("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$")
 print("Ready")
 
-run(host="::", port=8080, server='paste')
+run(host="127.0.0.1", port=8080, server='gunicorn')
