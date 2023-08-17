41a8816694805b21d1bad5f1e4fbd25535828589
Neoon <neo@tiefkuehler-madness.me>
2023-08-17 04:18:34 +0200

    add worker.sh

---
+++ worker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
+apt-get install python3 python3-pip fping git cron -y
+pip3 install pyasn requests
+cd /home/
+#git
+git clone https://github.com/Ne00n/llaas.git
+cd llaas
+cp worker.example.json worker.json
+useradd llaas -r -d /home/llas -s /bin/bash
+chown -R llaas:llaas /home/llaas/
+crontab -u llaas -l 2>/dev/null | { cat; echo \\"* * * * *  /home/llaas/worker.py > /dev/null 2>&1\\"; } | crontab -u llaas -
