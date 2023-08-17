20874738de3398c12e18cf111ac13d13d564f71d
Neoon <neo@tiefkuehler-madness.me>
2023-08-17 05:10:10 +0200

    update worker.sh

--- worker.sh
+++ worker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
 apt-get install python3 python3-pip fping git cron -y
-pip3 install pyasn requests
+pip3 install pyasn requests bottle gunicorn
 cd /home/
 git clone https://github.com/Ne00n/llaas.git
 cd llaas
