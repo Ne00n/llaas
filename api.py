1f297d35cfd327ecefce2ec458590ae9bcafa323
Neoon <neo@tiefkuehler-madness.me>
2023-08-21 01:46:04 +0200

    foreign deletion fix

--- api.py
+++ api.py
@@ -13,6 +13,13 @@ def validate(payload):
     for worker,details in config['workers'].items():
         if worker == payload['worker'] and details['token'] == payload['token']: return True
 
+def getConnection():
+    connection = sqlite3.connect("file:subnets?mode=memory&cache=shared", uri=True, isolation_level=None, timeout=10)
+    connection.execute('PRAGMA journal_mode = WAL;')
+    connection.execute('PRAGMA foreign_keys = ON;')
+    connection.commit()
+    return connection
+
 def query(request,pings):
     if len(request) > 100: return HTTPResponse(status=414, body={"data":"way to fucking long"})
     request = request.replace("/","")
@@ -22,9 +29,7 @@ def query(request,pings):
     if not result: return HTTPResponse(status=400, body={"data":"Invalid Amount of Pings"})
     asndata = asndb.lookup(ipv4[0])
     if asndata[0] is None: return HTTPResponse(status=400, body={"data":"invalid IPv4"})
-    connection = sqlite3.connect("file:subnets?mode=memory&cache=shared", uri=True, isolation_level=None, timeout=10)
-    connection.execute('PRAGMA journal_mode=WAL;')
-    connection.commit()
+    connection = getConnection()
     response = list(connection.execute("SELECT requests.subnet,requests.ip,results.worker,results.latency,requests.expiry FROM requests LEFT JOIN results ON requests.subnet = results.subnet WHERE requests.subnet = ? ORDER BY results.latency",(asndata[1],)))
     if response and int(time.time()) > int(response[0][4]):
         connection.execute(f"DELETE FROM requests WHERE subnet = ?",(response[0][0],))
@@ -41,26 +46,27 @@ def query(request,pings):
     connection.close()
     data = {}
     for row in response:
+        if not row[2] in data: data[row[2]] = {"pings":[]}
+        data[row[2]]["pings"].append(row[3])
         if row[3] is None: 
             data = {}
             break
-        data[row[2]] = row[3]
     return {"subnet":response[0][0],"ip":response[0][1],"data":data}
 
 @route('/job/get', method='POST')
 def index():
     payload = json.load(request.body)
     if not validate(payload): return HTTPResponse(status=401, body={"error":"Invalid Auth"})
+    connection = getConnection()
     ips = list(connection.execute("SELECT results.id,requests.subnet,requests.ip,results.worker FROM requests LEFT JOIN results ON requests.subnet = results.subnet WHERE results.worker = ? AND results.latency is NULL LIMIT 1000",(payload['worker'],)))
+    connection.close()
     return {"ips":ips}
 
 @route('/job/deliver', method='POST')
 def index():
     payload = json.load(request.body)
     if not validate(payload): return HTTPResponse(status=401, body={"error":"Invalid Auth"})
-    connection = sqlite3.connect("file:subnets?mode=memory&cache=shared", uri=True, isolation_level=None, timeout=10)
-    connection.execute('PRAGMA journal_mode = WAL;')
-    connection.commit()
+    connection = getConnection()
     for subnet,details in payload['data'].items():
         connection.execute(f"UPDATE results SET latency = ? WHERE subnet = ? and worker = ? and id = ?",(details['latency'],subnet,payload['worker'],details['id'],))
     connection.commit()
@@ -68,7 +74,7 @@ def index():
     return HTTPResponse(status=200, body={})
 
 @route('/<request>/<pings>', method='GET')
-def index(request='',pings=2):
+def index(request='',pings="2"):
     return query(request,pings)
 
 @route('/<request>', method='GET')
@@ -76,11 +82,9 @@ def index(request=''):
     return query(request,"2")
 
 print("Preparing sqlite3")
-connection = sqlite3.connect("file:subnets?mode=memory&cache=shared", uri=True, isolation_level=None)
-connection.execute("""CREATE TABLE requests (subnet, ip, expiry)""")
+connection = getConnection()
+connection.execute("""CREATE TABLE requests (subnet PRIMARY KEY, ip, expiry)""")
 connection.execute("""CREATE TABLE results (id INTEGER NOT NULL PRIMARY KEY, subnet, worker, latency DECIMAL(3,2) DEFAULT NULL, FOREIGN KEY(subnet) REFERENCES requests(subnet) ON DELETE CASCADE)""")
-connection.execute('PRAGMA journal_mode = WAL;')
-connection.execute('PRAGMA foreign_keys = ON;')
 connection.commit()
 print("Loading config")
 with open(f"{fullPath}configs/api.json") as f: config = json.load(f)
