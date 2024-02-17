23d0641e8a7f103bd6c1d20b4b681c14a7292558
Neoon <neo@tiefkuehler-madness.me>
2024-02-17 12:06:50 +0100

    add ID

--- dump.sql
+++ dump.sql
@@ -3,6 +3,7 @@ START TRANSACTION;
 SET time_zone = "+00:00";
 
 CREATE TABLE `requests` (
+  `ID` int(11) NOT NULL,
   `subnet` varchar(20) NOT NULL,
   `ip` varchar(15) NOT NULL,
   `expiry` int(11) NOT NULL
@@ -16,10 +17,15 @@ CREATE TABLE `results` (
 
 
 ALTER TABLE `requests`
+  ADD PRIMARY KEY (`ID`),
   ADD UNIQUE KEY `subnet` (`subnet`);
 
 ALTER TABLE `results`
   ADD UNIQUE KEY `subnet` (`subnet`,`worker`);
+
+
+ALTER TABLE `requests`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
 
 
 ALTER TABLE `results`
