75f3867ee78e02614f4b00735a13e143b52a5973
Neoon <neo@tiefkuehler-madness.me>
2024-02-19 16:11:30 +0100

    update for debian 12

--- worker.sh
+++ worker.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 if [[ $(id -u) -ne 0 ]] ; then echo "Please run as root" ; exit 1 ; fi
-apt-get install python3 python3-pip fping git cron -y
-pip3 install pyasn requests bottle gunicorn
+apt-get install python3 python3-requests fping git cron -y
 cd /home/
 git clone https://github.com/Ne00n/llaas.git
 cd llaas
