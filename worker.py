c297bc0ab320f1adf9c6ec4eec41c50ccf2961e7
Neoon <neo@tiefkuehler-madness.me>
2024-02-24 12:36:52 +0100

    batches

--- worker.py
+++ worker.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import requests, subprocess, json, time, ssl, sys, re, os
+import requests, subprocess, json, time, math, ssl, sys, re, os
 
 fullPath = os.path.realpath(__file__).replace("worker.py","")
 
@@ -27,32 +27,35 @@ def call(url,payload):
             print(f"Error {e}")
             error(run)
 
-runtime = 0
+runtime,batchSize = 0,100
 while runtime < 57:
     start = time.perf_counter()
     data = call(f"{config['api']}/job/get",config)
     print(f"Got {len(data['ips'])} IP's")
     if len(data['ips']) > 0:
-        ips,mapping = [],{}
-        for row in data['ips'][:100]:
-            ips.append(row['ip'])
-            mapping[row['ip']] = row
+        rounds = int(math.ceil(len(data['ips']) / batchSize))
+        for run in rounds:
+            prinnt(f"Running batch {run} of {rounds}")
+            ips,mapping = [],{}
+            for row in data['ips'][:batchSize]:
+                ips.append(row['ip'])
+                mapping[row['ip']] = row
 
-        fping = f"fping -c 1 "
-        fping += " ".join(ips)
+            fping = f"fping -c 1 "
+            fping += " ".join(ips)
 
-        p = subprocess.run(fping, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        parsed = re.findall("(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*?(min/avg/max) = [0-9.]+/([0-9.]+)",p.stderr.decode('utf-8'), re.MULTILINE)
+            p = subprocess.run(fping, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+            parsed = re.findall("(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}).*?(min/avg/max) = [0-9.]+/([0-9.]+)",p.stderr.decode('utf-8'), re.MULTILINE)
 
-        response = config
-        response["data"] = {}
-        for row in parsed:
-            currentIP = row[0]
-            subnet = mapping[currentIP]['subnet']
-            response["data"][subnet] = {"ip":currentIP,"latency":row[2]}
+            response = config
+            response["data"] = {}
+            for row in parsed:
+                currentIP = row[0]
+                subnet = mapping[currentIP]['subnet']
+                response["data"][subnet] = {"ip":currentIP,"latency":row[2]}
 
-        for row in data['ips'][:100]:
-            if not row['subnet'] in response['data']: response["data"][row['subnet']] = {"id":row['ID'],"ip":row['ip'],"latency":-1}
+            for row in data['ips'][:batchSize]:
+                if not row['subnet'] in response['data']: response["data"][row['subnet']] = {"id":row['ID'],"ip":row['ip'],"latency":-1}
 
         data = call(f"{config['api']}/job/deliver",response)
     elif runtime < 50: time.sleep(10)
